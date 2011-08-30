bbaecdd8e0eeb7fb8488dd06cb6ed3c089cd3922
Peter van Kampen <pterk@datatailors.com>
2011-08-30 21:09:55 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcc',
-    version="0.2",
+    version="0.3",
     description="Simple but effective comments app",
     #long_description=open('readme').read(),
     keywords='',
